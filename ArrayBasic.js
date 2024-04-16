e4c6e6457a9c432e65fe0a62cf094dfd7fdd8626
root <root@Unaislaptop>
2024-04-16 12:47:09 +0530

    added two problem in arrayBasic

--- ArrayBasic.js
+++ ArrayBasic.js
@@ -15,7 +15,7 @@ function largest(arr){
     }
     return -1;
 }
-
+console.log('Lragest Number Index Naive solution');
 console.log(largest([5, 10, 8, 600, 30, 80])); 
 console.log('-------------------------------');
 
@@ -31,7 +31,38 @@ function largest1(arr){
     return largest
     
 }
-
+console.log('Lragest Number Index Efficient solution');
 console.log(largest1([5, 10, 8, 600, 30, 80])); 
 console.log('-------------------------------');
 
+// Check array is sorted in Naive way
+
+function isSorted(arr){
+    for(let i=0; i<arr.length; i++){
+        for(let j =i+1; j<arr.length ; j++){
+            if(arr[j]< arr[i]){
+                return false
+            }
+        }
+    }
+    return true
+}
+console.log('Finding sorted or not Naive solution');
+console.log(isSorted([7,20,30,10]));
+console.log('-------------------------------');
+
+
+// Efficient solution Check array is sorted
+
+function isSorted1(arr){
+    for(let i=1; i<arr.length; i++){
+        if(arr[i]< arr[i-1]){
+            return false
+        }
+    }
+    return true
+    
+}
+console.log('Finding sorted or not Efficient solution');
+console.log(isSorted1([7,20,30,10]));
+console.log('-------------------------------');
