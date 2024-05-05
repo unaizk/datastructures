2d075fa8d348cfa4120688285c1c4a7fde16942b
root <root@Unaislaptop>
2024-05-05 11:25:10 +0530

    added selection sort

---
+++ Sorting/SelectionSort/selectionSorting.js
@@ -0,0 +1,17 @@
+let arr = [5,90,50,1];
+
+function selectionSOrt(arr){
+    for(let i = 0; i<arr.length-1; i++){
+        let minIndex = i;
+
+        for(let j= i+1; j<arr.length; j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex = j
+            }
+        }
+        [arr[minIndex],arr[i]] = [arr[i],arr[minIndex]]
+    }
+    return arr
+};
+
+console.log(selectionSOrt(arr));
