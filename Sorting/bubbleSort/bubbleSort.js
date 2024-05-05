470e5baaed52592d341f51e541245838e798f342
root <root@Unaislaptop>
2024-05-05 10:50:55 +0530

    added bubbleSort

---
+++ Sorting/bubbleSort/bubbleSort.js
@@ -0,0 +1,15 @@
+let arr = [10,8,20,5];
+
+
+function bubbleSort(arr){
+    for(let i =0 ; i<arr.length-1; i++){
+        for(j=0; j<arr.length-1-i; j++){
+            if(arr[j]>arr[j+1]){
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort(arr));
